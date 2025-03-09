3f3f339b7eb6750b3cb85d560cb0ac7e15d23ab8
Eric Branson <eric.branson@gmail.com>
2025-03-09 14:07:13 -0600

    init

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   interface Props {
+      title: string
+   }
+   let { title }:Props = $props();
+   
+</script>
+<svelte:head>
+	<title>Stich Counter | {title}</title>
+</svelte:head>
