c84aa9ba7a937902d7dd400b0c7fc256798d0d97
Eric Branson <eric.branson@gmail.com>
2025-03-09 14:03:54 -0600

    installed skeleton

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,24 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { PartialUser } from "$lib/server/partialTypes";
+import type { Session } from "@prisma/client";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: PartialUser | null;
+			session: Session | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
+	const THEMES: {
+		name: string,
+		content: string
+	}[];
 }
 
 export {};
