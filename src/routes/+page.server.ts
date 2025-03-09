fefa923ca42900fbe8b2f4a26318ba2b72a0911d
Eric Branson <eric.branson@gmail.com>
2025-03-09 14:04:15 -0600

    towards a front page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,16 @@
 import { prisma } from '$lib/server/prisma';
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load = (async () => {
     const projects = prisma.project.findMany()
     return { projects };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+    newProject: async(event) => {
+        if(!event.locals.user){
+            redirect(302,'/login')
+        }
+    }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+   import { Combobox } from "@skeletonlabs/skeleton-svelte"
+   import type { Project } from "@prisma/client";
+   import type { PageData } from "./$types";
+
+   interface Props {
+      data: PageData
+   }
+   let {
+      data
+   }:Props = $props()
+   interface ComboboxData {
+      label: string;
+      value: string;
+   }
+   let projectSelect = $state([''])
+   let projectComboBoxData:ComboboxData[] = [];
+
+   const wrapper = new Promise<Project[]>(async res => {
+      const projects = await data.projects
+      projects.forEach((project) => {
+         const datum:ComboboxData = {
+            label: project.name,
+            value: project.id
+         }
+         projectComboBoxData.push(datum)
+      })
+      res(projects)
+   })
+</script>
+
+{#await wrapper}
+   <div>Loading projects</div>
+{:then projects} 
+   <div class="m-2">
+      <Combobox
+         data={projectComboBoxData}
+         value={projectSelect}
+         label='Projects'
+         placeholder='Select...'
+         openOnClick={true}
+      />
+   </div>
+{/await}
