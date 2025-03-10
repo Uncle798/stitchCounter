56ec824a430109383fe9ff2991285c3cebd4f931
Eric Branson <eric.branson@gmail.com>
2025-03-10 08:31:06 -0600

    displays everything

--- src/routes/projects/[projectId]/+page.svelte
+++ src/routes/projects/[projectId]/+page.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
+    import StitchDisplay from '$lib/displayComponents/StitchDisplay.svelte';
+   import NewPatternForm from '$lib/forms/NewPatternForm.svelte';
+
+   import NewRowForm from '$lib/forms/NewRowForm.svelte';
+    import ProjectDeleteForm from '$lib/forms/ProjectDeleteForm.svelte';
    import Header from '$lib/Header.svelte';
    import type { PageData } from './$types';
 
    let { data }: { data: PageData } = $props();
 </script>
-<div>
+<a href="/" class="anchor">Home</a>
+{#if data.project}
+   
+<div class="m-2">
    <Header title={data.project!.name} />
-   <h1>{data.project?.name}</h1>
+   <h1 class="h1">{data.project?.name}</h1>
    {#each data.rows as row}
-      {row}
+   {@const {stitches} = row}
+   Row number {row.number}
+   {#each stitches as stitch}
+      <StitchDisplay {stitch} />
+   {/each}
+   <NewPatternForm rowId={row.rowId} data={data.newPatternForm}/>
    {/each}
 </div>
+   <NewRowForm projectId={data.project?.id} data={data.newRowForm} rowNumber={data.rows.length+1}/>
+
+   <ProjectDeleteForm projectId={data.project.id} projectName={data.project.name} data={data.projectDeleteForm}/>
+{/if}
