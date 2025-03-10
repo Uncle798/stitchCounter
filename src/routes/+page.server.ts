9b8426af34ef198c12eeb2595e300a9069902549
Eric Branson <eric.branson@gmail.com>
2025-03-10 08:29:51 -0600

    returns new project form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,14 @@
 import { prisma } from '$lib/server/prisma';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { newProjectFormSchema } from '$lib/formSchemas/schemas';
 
 export const load = (async () => {
-    const projects = prisma.project.findMany()
-    return { projects };
+    const projects = prisma.project.findMany();
+    const newProjectForm = await superValidate(zod(newProjectFormSchema));
+    return { projects, newProjectForm };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
