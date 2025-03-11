57f7cd04809cc97d2da45c08673c9ec035fe0603
Eric Branson <eric.branson@gmail.com>
2025-03-11 13:50:55 -0600

    added main menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,56 @@
 <script lang="ts">
+	import { Modal } from '@skeletonlabs/skeleton-svelte';
 	import '../app.css';
-	let { children } = $props();
+	import { beforeNavigate } from '$app/navigation';
+	import {Menu} from 'lucide-svelte';
+	import { enhance } from '$app/forms';
+	let { children, data } = $props();
+	interface Link {
+		link: string;
+		label: string;
+	}
+	const navLinks:Link[] = [
+		{label:'Home', link: '/'},
+
+	]
+	let menuOpen = $state(false);
+	beforeNavigate(()=> {
+		menuOpen=false
+	})
 </script>
 
+<Modal
+	open={menuOpen}
+	onOpenChange={(event) => (menuOpen = event.open)}
+	triggerBase="btn bg-primary-50 dark:bg-primary-950 hover:shadow-xl hover:border-2 border-secondary-50 dark:border-secondary-950 fixed top-0 z-50"
+	contentBase="bg-surface-100-900 p-2 space-y-2 shadow-xl w-[240px] h-screen"
+	positionerJustify="justify-start"
+	positionerAlign=""
+	positionerPadding=""
+	transitionsPositionerIn={{ x: -240, duration: 400 }}
+	transitionsPositionerOut={{ x: -240, duration: 400 }}
+>
+	{#snippet trigger()}
+		<Menu class="mx-2 border-2" aria-label='Main Menu'/>
+	{/snippet}
+	{#snippet content()}		
+	<div>
+		<ul>
+			{#each navLinks as link}
+				<li><a href={link.link} class="anchor">{link.label}</a></li>
+			{/each}
+			<div class="absolute bottom-0 m-1 sm:m-2 mb-2">
+				{#if data.user}
+				<li><a href="/accountSettings" class="anchor">Settings</a></li>
+					<form action="/logout" method="post" use:enhance>
+						<li><button class="anchor" type="submit">Logout</button></li>
+					</form>
+				{:else}
+					<li><a class="anchor" href="/login">Login</a></li>
+				{/if}
+			</div>
+		</ul>
+	</div>
+	{/snippet}
+</Modal>
 {@render children()}
