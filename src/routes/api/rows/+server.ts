1bf1f82be9d7a3eb6602c547d9f2d7cf662fde20
Eric Branson <eric.branson@gmail.com>
2025-03-13 12:16:18 -0600

    init

---
+++ src/routes/api/rows/+server.ts
@@ -0,0 +1,68 @@
+import { prisma } from '$lib/server/prisma';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async (event) => {
+    const body = await event.request.json();
+    const { rowId } = body;
+    if(rowId){
+        const row = await prisma.row.findUnique({
+            where: {
+                rowId
+            }
+        })
+        if(row){
+            return new Response(JSON.stringify(row), {status: 200})
+        }
+        return new Response(JSON.stringify('Row not found'), {status: 404})
+    }
+    return new Response(JSON.stringify('Row Id not provided'), {status: 400});
+};
+
+export const DELETE: RequestHandler = async (event) => {
+    const body = await event.request.json();
+    const { rowId } = body;
+    if(rowId){
+        const row = await prisma.row.findUnique({
+            where: {
+                rowId
+            }
+        })
+        if(row){
+            await prisma.row.delete({
+                where: {
+                    rowId: row.rowId
+                }
+            })
+            return new Response(JSON.stringify('Row deleted'), {status: 200})
+        }
+        return new Response(JSON.stringify('Row not found'), {status: 404})
+    }
+    return new Response(JSON.stringify('Row Id not provided'), {status: 400})
+};
+
+export const POST: RequestHandler = async (event) => {
+    const body = await event.request.json();
+    const { projectId } = body
+    if(projectId){
+        const project = await prisma.project.findUnique({
+            where: {
+                id: projectId
+            }
+        });
+        if(project){
+            const rowCount = await prisma.row.count({
+                where: {
+                    projectId: project.id
+                }
+            })
+            const row = await prisma.row.create({
+                data:{
+                    projectId: project.id,
+                    number: rowCount + 1,
+                }
+            })
+        }
+        return new Response(JSON.stringify('Project not found'), {status: 404})
+    }
+    return new Response(JSON.stringify('Project Id not provided'), {status: 400})
+}
